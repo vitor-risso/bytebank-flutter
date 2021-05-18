f90be80ee64a1eabbf89754b22fc89c79c14ad81
vitor-risso <vitor.risso13@gmail.com>
2021-05-18 09:03:59 -0300

    [CLASS] creating classes to let the app readable

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,18 +12,103 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: Text("ola"),
+      home: TransferForms(),
     );
   }
+}
+
+class TransferForms extends StatelessWidget {
+  final TextEditingController _value = TextEditingController();
+  final TextEditingController _accountNumber = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Criando transferencia"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _accountNumber,
+              style: TextStyle(
+                fontSize: 24,
+              ),
+              decoration: InputDecoration(
+                  labelText: "Numero da conta", hintText: "0000"),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _value,
+              style: TextStyle(
+                fontSize: 24,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: "Valor",
+                  hintText: "00.00"),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              final double value = double.tryParse(_value.text);
+              final int accountNUmber = int.tryParse(_accountNumber.text);
+
+              if (vlaue != null && accountNUmber != null) {
+                Transfer(value., accountNUmber)
+              }
+            },
+            child: Text("Confirmar"),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [TransferItem(Transfer(1.23, 32123))],
+      ),
+      appBar: AppBar(title: Text("Tranferencias")),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int accountNumber;
+
+  Transfer(this.value, this.accountNumber);
 }
