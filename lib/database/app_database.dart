608fd32b6c6058c624128106586a7f5789e57775
vitor-risso <vitor.risso13@gmail.com>
2021-05-20 18:25:34 -0300

    [DAO] creating dao and some const

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,4 +1,4 @@
-import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/database/DAO/contact_dao.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -8,34 +8,9 @@ Future<Database> createDataBase() async {
   return openDatabase(
     path,
     onCreate: (db, version) {
-      db.execute("CREATE TABLE contacts ("
-          "id INTEGER PRIMARY KEY,"
-          "name TEXT,"
-          "account_number INTEGER )");
+      db.execute(ContactDAO.tableSQL);
     },
     version: 1,
     onDowngrade: onDatabaseDowngradeDelete,
   );
-}
-
-Future<int> save(Contact contact) async {
-  final Database db = await createDataBase();
-  final Map<String, dynamic> contactMap = Map();
-  contactMap["name"] = contact.name;
-  contactMap["account_number"] = contact.accountNumber;
-  return db.insert("contacts", contactMap);
-}
-
-Future<List<Contact>> findAll() async {
-  final Database db = await createDataBase();
-  final List<Map<String, dynamic>> result = await db.query("contacts");
-  final List<Contact> contacts = [];
-  for (Map<String, dynamic> row in result) {
-    final Contact contact = new Contact(
-      row["name"],
-      row["account_number"],
-    );
-    contacts.add(contact);
-  }
-  return contacts;
 }
