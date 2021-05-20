eb08e50810975066bea11eaa5b008a236c538c95
vitor-risso <vitor.risso13@gmail.com>
2021-05-20 14:55:37 -0300

    [GESTURE] adding tap to an elemnte who does not has tap property

--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/screens/contacts_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -18,28 +19,39 @@ class Dashboard extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              height: 100,
-              width: 150,
+            child: Material(
               color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(
-                    Icons.people,
-                    size: 24.0,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    "Contact",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ContactsList(),
                     ),
-                  )
-                ],
+                  );
+                },
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  height: 100,
+                  width: 150,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.people,
+                        size: 24.0,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "Contact",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
           )
