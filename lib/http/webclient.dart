e6090dfb40bc62924f43be2f761143c39cbe02cd
vitor-risso <vitor.risso13@gmail.com>
2021-05-21 17:47:05 -0300

    [API] return response from api in a json fromat

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,8 +1,40 @@
-import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart';
+import 'dart:convert';
 
-void findAll() async {
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/models/transaction.dart';
+import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print(data.toString());
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print(data.body);
+    return data;
+  }
+}
+
+Future<List<Transaction>> findAll() async {
+  final Client client =
+  HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
   final Response response =
-  await get(Uri(scheme: "http://179.159.39.85:8080/transactions"));
-  debugPrint(response.body);
+  await client.get(Uri.parse("http://192.168.0.122:8080/transactions"));
+
+  final List<dynamic> jsonList = jsonDecode(response.body);
+  final List<Transaction> transactions = [];
+
+  for (Map<String, dynamic> element in jsonList) {
+    final Transaction transaction = Transaction(
+      element['value'],
+      Contact(element['contact']['name'], element['contact']['accountNumber']),
+    );
+    transactions.add(transaction);
+  }
+
+  return transactions;
 }
