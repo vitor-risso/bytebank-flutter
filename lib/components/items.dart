15baaa294fa3b4bb267cb87743daa80c48b33f0d
vitor-risso <vitor.risso13@gmail.com>
2021-05-21 20:49:20 -0300

    [CARD ITEM] adding a property to send a function when call the card item to do the navigation

--- lib/components/items.dart
+++ lib/components/items.dart
@@ -4,13 +4,15 @@ import 'package:flutter/material.dart';
 
 class ContactItem extends StatelessWidget {
   final Contact _transfer;
+  final Function onCLick;
 
-  ContactItem(this._transfer);
+  ContactItem(this._transfer, {@required this.onCLick});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
+        onTap: ()=> onCLick(),
         title: Text(_transfer.name.toString()),
         subtitle: Text(_transfer.accountNumber.toString()),
       ),
