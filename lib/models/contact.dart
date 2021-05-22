b67bb9ff8d9d7b7e4858047746cf22446c35d122
vitor-risso <vitor.risso13@gmail.com>
2021-05-21 21:39:01 -0300

    [REAFCTOR] creating functions toJson and fromJson

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -2,5 +2,15 @@ class Contact {
   final String name;
   final int accountNumber;
 
-  Contact( this.name, this.accountNumber);
+  Contact(this.name, this.accountNumber);
+
+  Contact.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        accountNumber = json['accountNumber'];
+
+  Map<String, dynamic> toJson() =>
+      {
+        "name": name,
+        "accountNumber": accountNumber
+      };
 }
