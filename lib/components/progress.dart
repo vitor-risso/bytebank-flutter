0816896f76c89683ff21650a30cb041a8f678738
vitor-risso <vitor.risso13@gmail.com>
2021-05-24 18:49:31 -0300

    [PROGRESS] adding property msg to progress component with "loading" as default text

--- lib/components/progress.dart
+++ lib/components/progress.dart
@@ -2,13 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Progress extends StatelessWidget {
+  final String msg;
+
+  Progress({this.msg = "Loading..."});
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [CircularProgressIndicator(), Text("Carregando")],
+        children: [CircularProgressIndicator(), Text(msg)],
       ),
     );
   }
